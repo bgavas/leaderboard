3d1a942ea38063245229a292b7f8155f27ca674d
burak <burak.gavas@misonesolution.com>
2019-11-08 20:41:57 +0300

    Add db connection

---
+++ server/db/connectDb.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(
+    process.env.DATABASE_DB,
+    process.env.DATABASE_USER,
+    process.env.DATABASE_PASSWORD,
+    {
+        host: process.env.DATABASE_HOST,
+        dialect: process.env.DATABASE_DIALECT,
+        operatorsAliases: '',
+        logging: false
+    }
+);
+
+// Test the connection to database
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.log('Unable to connect to the database:', err);
+    });
+
+// Export
+module.exports = sequelize;
--- server/server.js
+++ server/server.js
@@ -4,3 +4,26 @@ const { AVAILABLE_VERSIONS, EXPOSED_HEADERS, ENVIRONMENT, TIMEOUT } = require('.
 require('./config/config.js');
 // Init helper functions
 require('./util/helper');
+// Connect to db
+require('./db/connectDb');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const initializeSwagger = require('./util/swagger');
+
+const app = express();
+const port = process.env.PORT;
+
+// App middleware
+app.use(bodyParser.json());
+
+// Initialize swagger
+initializeSwagger(app);
+
+// Start server
+app.listen(port, () => {
+    console.log(`App started on port ${port}`);
+});
+
+// Export for testing
+module.exports = { app };
