97518bd57719023d2a757034a7674bdeab5d0ada
burak <burak.gavas@misonesolution.com>
2019-11-08 20:31:29 +0300

    Add constant file

---
+++ server/util/constant.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+    AVAILABLE_VERSIONS: ['v1'], // Available REST service versions
+
+    ENVIRONMENT: {
+        TEST: 'test',
+        DEVELOPMENT: 'development',
+        PRODUCTION: 'production'
+    },
+
+    RESPONSE_STATUS: {
+        FAIL: 'Fail',
+        SUCCESS: 'Success',
+        AUTHENTICATION_FAILED: 'Authentication fail'
+    },
+
+};
