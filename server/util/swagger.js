cc1278030d929f94714e4c577097625808f566c2
burak <burak.gavas@misonesolution.com>
2019-11-08 20:30:21 +0300

    Add swagger util

---
+++ server/util/swagger.js
@@ -0,0 +1,41 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+// swagger definition
+const swaggerDefinition = {
+    info: {
+        title: 'Leaderboard Node Swagger API',
+        version: '1.0.0',
+        description: 'Leaderboard Swagger',
+    },
+    host: process.env.API_URL.replace('http://', '').replace('https://', ''),
+    basePath: '/',
+    securityDefinitions: {},
+    security: [
+        {}
+    ]
+};
+
+// Options for the swagger docs
+var options = {
+    // import swaggerDefinitions
+    swaggerDefinition: swaggerDefinition,
+    // path to the API docs
+    apis: ['./server/controller/*/*/*.js']
+};
+
+// initialize swagger-jsdoc
+const swaggerSpec = swaggerJSDoc(options);
+
+module.exports = (app) => {
+
+    // Swagger.json get
+    app.get('/swagger.json', (req, res) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(swaggerSpec);
+    });
+
+    // Initialize ui
+    app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+}
