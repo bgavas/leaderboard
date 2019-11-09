a341b883d0af85ca9c4cdf534f57fcdb06986ef2
burak <burak.gavas@misonesolution.com>
2019-11-09 21:07:55 +0300

    Fix

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,6 @@
 const { ENVIRONMENT } = require('./../util/constant');
 
 var env = process.env.NODE_ENV || ENVIRONMENT.DEVELOPMENT;
-env = 'production';
 process.env.NODE_ENV = env;
 
 var config = require('./config.json');
